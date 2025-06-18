34f0b3ba8b7711bc488fbcc7b9d17e38094c67ca
Henry Luna <lunaretornado2@gmail.com>
2025-06-17 21:42:05 -0300

    pag principal, login, y registro

---
+++ data/dataBaseLogin.js
@@ -0,0 +1,11 @@
+export const usuario = [
+  {
+    id: 1,
+    nombre: "admin",
+    apellido: "admin",
+    email: "luis@chefencasa.com",
+    usuario: "admin",
+    contrasena: "admin123",
+    rol: "admin",
+  },
+];
